8f81e83f289e14505a6624d58973781d96b03655
Adebanjo Oluwasola <adebanjo.oluwasola.37@gmail.com>
2022-06-30 13:29:27 +0100

    Refactoring the Relationship

---
+++ StyleONApi/StyleONApi/Controllers/SellersController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StyleONApi.Entities;
+using StyleONApi.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StyleONApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SellersController : ControllerBase
+    {
+        IProductRepository _repository;
+        public SellersController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Seller>>> GetAllSellers()
+        {
+            var result = await _repository.GetAllSellers();
+            return Ok(result);
+        }
+            
+    }
+}
---
+++ StyleONApi/StyleONApi/Entities/Seller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StyleONApi.Entities
+{
+    public class Seller
+    {
+        [Required]
+        public Guid SellerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string HouseAddress { get; set; }
+        public string State { get; set; }
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+    }
+}
